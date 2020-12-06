a22a8503827e0deb1baf68d35a8fc97145856193
Shermin Chai <74152272+SherminChai@users.noreply.github.com>
2020-12-06 13:45:04 +0800

    Adding the Ticketing and FAQ segment in index.html, script.js and style.css

--- uol-web-dev/script.js
+++ uol-web-dev/script.js
@@ -1,2 +1,38 @@
 // This comment is here because file cannot be empty
 // Do ensure that you variable names are all unique and meaningful
+
+//FAQ script
+ var acc = document.getElementsByClassName("accordion");
+    var i;
+
+    for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+
+    var panel = this.nextElementSibling;
+        if (panel.style.display === "block") {
+            panel.style.display = "none";
+        } else {
+            panel.style.display = "block";
+        }
+    });
+    }
+
+//Ticketing style
+function clickHandle(evt, ticketing) {
+    var i, tabcontent, tablinks;
+
+    //clear the previous clicked content
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    //set the tab to be "active"
+    tablinks = document.getElementsByClassName('"tablinks"');
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace("active", " ");
+    }
+    //display the clicked tab and set it to active
+    document.getElementById(ticketing).style.display = "block";
+        evt.currentTarget.classname += "active";
+    }
