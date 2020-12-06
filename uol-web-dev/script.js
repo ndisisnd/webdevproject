1a198d39c2044fe5cfd14806a5c4679001d2f14b
lelouchx666 <74188533+lelouchx666@users.noreply.github.com>
2020-12-06 17:02:51 +0800

    Adding 3 maps with functionality in Marvel & DC

--- uol-web-dev/script.js
+++ uol-web-dev/script.js
@@ -1,2 +1,23 @@
 // This comment is here because file cannot be empty
 // Do ensure that you variable names are all unique and meaningful
+function toggleVisibility(selectedTab) {
+  var content = document.getElementsByClassName("mmapcontent");
+  for (var i = 0; i < content.length; i++) {
+    if (content[i].id == selectedTab) {
+      content[i].style.display = "block";
+    } else {
+      content[i].style.display = "none";
+    }
+  }
+}
+
+function toggleVisibility(selectedTab) {
+  var content = document.getElementsByClassName("dcmapcontent");
+  for (var i = 0; i < content.length; i++) {
+    if (content[i].id == selectedTab) {
+      content[i].style.display = "block";
+    } else {
+      content[i].style.display = "none";
+    }
+  }
+}
